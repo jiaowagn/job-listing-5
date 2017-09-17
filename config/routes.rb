34a2d7b492d6346b3b3d3c1742adb965aaac7d8e
jiaowagn <wangjiaohere@gmail.com>
2017-09-17 13:18:07 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :jobs do
-    resources :resumes 
+    resources :resumes
   end
 
   namespace :admin do
     resources :jobs  do
+      resources :resumes
       member do
         post :hide
         post :publish
